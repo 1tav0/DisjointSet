23ca7b0219ecbcd86d118a6b50e84e08afdda50c
Tavo Cruz <madecruz2@gmail.com>
2023-07-13 21:56:32 -0700

    dp on strings

--- printLcs.cpp
+++ printLcs.cpp
@@ -19,27 +19,27 @@ void lcs(string s, string t){
         }
     }
 
-    int len = dp[n][m];
-    string ans = "";
-    for (int i = 0; i < len; i++){
-        ans[i] = '$';
-    }
+    // int len = dp[n][m];
+    // string ans = "";
+    // for (int i = 0; i < len; i++){
+    //     ans[i] = '$';
+    // }
 
-    int ind = len - 1;
-    int i = n, j = m;
-    while(i>0 && j>0){
-        if(s[i-1]==t[j-1]){
-            ans[ind] = s[i-1];
-            ind--;
-            i--;
-            j--;
-        }else if(dp[i-1][j]>dp[i][j-1]){
-            i--;
-        }else{
-            j--;
-        }
-    }
-    cout << ans;
+    // int ind = len - 1;
+    // int i = n, j = m;
+    // while(i>0 && j>0){
+    //     if(s[i-1]==t[j-1]){
+    //         ans[ind] = s[i-1];
+    //         ind--;
+    //         i--;
+    //         j--;
+    //     }else if(dp[i-1][j]>dp[i][j-1]){
+    //         i--;
+    //     }else{
+    //         j--;
+    //     }
+    // }
+    // cout << ans;
 }
 
 int main(){
@@ -53,4 +53,38 @@ int main(){
     lcs(s1, s2);
 
     return 0;
+}
+
+void printLcs(string s, string t){
+    int n = s.size();
+    int m = t.size();
+
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if(s[i-1]==t[j-1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    int len = dp[n][m];
+    string ans = "";
+    for (int i = 0; i < len; i++){
+        ans += "$";
+    }
+    int ind = len - 1;
+    int i = n, j = m;
+    while(i>0 && j>0){
+        if(s[i-1]==t[j-1]){
+            ans[i] = s[i - 1];
+            ind--, i--, j--;
+        }else if(dp[i-1][j]>dp[i][j-1]){
+            i--;
+        }else{
+            j--;
+        }
+    }
+    cout << ans;
 }
