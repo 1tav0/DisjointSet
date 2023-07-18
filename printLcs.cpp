6998389df7ff735ee4be5227d762dbc9d98c8e61
Tavo Cruz <madecruz2@gmail.com>
2023-07-17 17:48:27 -0700

    print lcs

--- printLcs.cpp
+++ printLcs.cpp
@@ -7,7 +7,7 @@ void lcs(string s, string t){
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     for (int j = 0; j <=m; j++)
         dp[0][j] = 0;
-    for (int i = 0; i <= m; i++)
+    for (int i = 0; i <= n; i++)
         dp[i][0] = 0;
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
@@ -55,35 +55,39 @@ int main(){
     return 0;
 }
 
-void printLcs(string s, string t){
+
+string printLcs(string s, string t){
     int n = s.size();
     int m = t.size();
-    vector<vector<int>> dp(n, vector<int>(m, 0));
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
-            if(s[i-1]==t[j-1]){
+            if(s[i-1] == t[j-1]){
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             }else{
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
+
     int len = dp[n][m];
     string ans = "";
     for (int i = 0; i < len; i++){
-        ans += "$";
+        ans[i] = '$';
     }
-    int ind = len - 1;
     int i = n, j = m;
+    int ind = len - 1;
     while(i>0 && j>0){
         if(s[i-1]==t[j-1]){
-            ans[ind] += s[i - 1];
-            ind--, i--, j--;
+            ans[ind] = s[i - 1];
+            ind--;
+            i--, j--;
         }else if(dp[i-1][j]>dp[i][j-1]){
             i--;
         }else{
             j--;
         }
     }
-    cout << ans;
+    return ans;
 }
