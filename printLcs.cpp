8f7ae2eca828cfd86be57457b86b01170182bcf6
Tavo Cruz <madecruz2@gmail.com>
2023-07-20 22:14:57 -0700

    comment

--- printLcs.cpp
+++ printLcs.cpp
@@ -56,38 +56,39 @@ int main(){
 }
 
 
-string printLcs(string s, string t){
-    int n = s.size();
-    int m = t.size();
+// string printLcs(string s, string t){
+//     int n = s.size();
+//     int m = t.size();
 
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= m; j++){
-            if(s[i-1] == t[j-1]){
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            }else{
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     for (int i = 1; i <= n; i++){
+//         for (int j = 1; j <= m; j++){
+//             if(s[i-1] == t[j-1]){
+//                 dp[i][j] = 1 + dp[i - 1][j - 1];
+//             }else{
+//                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+//     }
 
-    int len = dp[n][m];
-    string ans = "";
-    for (int i = 0; i < len; i++){
-        ans[i] = '$';
-    }
-    int i = n, j = m;
-    int ind = len - 1;
-    while(i>0 && j>0){
-        if(s[i-1]==t[j-1]){
-            ans[ind] = s[i - 1];
-            ind--;
-            i--, j--;
-        }else if(dp[i-1][j]>dp[i][j-1]){
-            i--;
-        }else{
-            j--;
-        }
-    }
-    return ans;
-}
+//     int len = dp[n][m];
+//     string ans = "";
+//     for (int i = 0; i < len; i++){
+//         ans[i] = '$';
+//     }
+//     int i = n, j = m;
+//     int ind = len - 1;
+//     while(i>0 && j>0){
+//         if(s[i-1]==t[j-1]){
+//             ans[ind] = s[i - 1];
+//             ind--;
+//             i--, j--;
+//         }else if(dp[i-1][j]>dp[i][j-1]){
+//             i--;
+//         }else{
+//             j--;
+//         }
+//     }
+//     return ans;
+// }
+
